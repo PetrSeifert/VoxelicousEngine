5094ba82f1341f26e2617fa0461f820f5d952631
PetrSeifert <petr.seifert02@gmail.com>
2023-04-27 02:09:17 +0200

    Basic spdlog module implementation

--- VoxelicousEngine/src/VoxelicousEngine/Core/EntryPoint.h
+++ VoxelicousEngine/src/VoxelicousEngine/Core/EntryPoint.h
@@ -1,11 +1,12 @@
-#pragma once
-
 #ifdef VE_PLATFORM_WINDOWS
 
 extern VoxelicousEngine::App* VoxelicousEngine::CreateApp();
 
 int main(int argc, char** argv)
 {
+	VoxelicousEngine::Log::Init();
+	VE_CORE_WARN("Init Log!");
+
 	auto app = VoxelicousEngine::CreateApp();
 	app->Run();
 	delete app;
